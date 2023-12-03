bff2a953cc675aab2e8958f86e7014d2aa8b0bff
ricky <{ID}+{username}@users.noreply.github.com>
2023-12-03 01:23:24 -0500

    disable tilt in ios

--- components/card.jsx
+++ components/card.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import Tilt from "@/components/tilt";
 import ReactCardFlip from "react-card-flip";
+import * as rdd from "react-device-detect";
 
 const Card = ({ frontComponent, backComponent }) => {
   const [isFlipped, setFlip] = useState(false);
@@ -26,28 +27,46 @@ const Card = ({ frontComponent, backComponent }) => {
   };
 
   return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-      <div onClick={flipCard}>
-        <Tilt
-          options={{ glare: true, "max-glare": 0.42, perspective: 690 }}
-          className=" rounded-[36px] w-full h-full cursor-pointer"
-        >
-          <div className="w-full h-full" key="front">
-            {frontComponent}
+    <>
+      {rdd.isIOS ? (
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div onClick={flipCard}>
+            <div className="rounded-[36px] w-full h-full cursor-pointer" key="front">
+              {cardFaces.front}
+            </div>
           </div>
-        </Tilt>
-      </div>
-      <div onClick={flipCard}>
-        <Tilt
-          options={{ glare: true, "max-glare": 0.42, perspective: 690 }}
-          className="rounded-[36px] w-full h-full cursor-pointer"
-        >
-          <div className="w-full h-full" key="back">
-            {backComponent}
+
+          <div onClick={flipCard}>
+            <div className="rounded-[36px] w-full h-full cursor-pointer" key="back">
+              {cardFaces.back}
+            </div>
           </div>
-        </Tilt>
-      </div>
-    </ReactCardFlip>
+        </ReactCardFlip>
+      ) : (
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div onClick={flipCard}>
+            <Tilt
+              options={{ glare: true, "max-glare": 0.42, perspective: 690 }}
+              className="rounded-[36px] w-full h-full cursor-pointer"
+            >
+              <div className="w-full h-full" key="front">
+                {frontComponent}
+              </div>
+            </Tilt>
+          </div>
+          <div onClick={flipCard}>
+            <Tilt
+              options={{ glare: true, "max-glare": 0.42, perspective: 690 }}
+              className="rounded-[36px] w-full h-full cursor-pointer"
+            >
+              <div className="w-full h-full" key="back">
+                {backComponent}
+              </div>
+            </Tilt>
+          </div>
+        </ReactCardFlip>
+      )}
+    </>
   );
 };
 
